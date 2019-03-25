f8389b6a717a8bfd12c6191a586662545b6404a5
Thorpe Lee <koangbok@gmail.com>
2019-03-26 00:37:33 +0900

    template path update

--- src/Core/Config.php
+++ src/Core/Config.php
@@ -44,8 +44,8 @@ class Config
             define("LOG_LEVEL", $setting['log']['level']);
             define("LOG_PATH", $setting['log']['path']);
 
-            define("VIEW_PATH_PHP", $setting['template']['output']);
-            define("VIEW_PATH_HTML", $setting['template']['input']);
+            define("VIEW_PATH_PHP", $setting['project']['path'] . $setting['template']['output']);
+            define("VIEW_PATH_HTML", $setting['project']['path'] . $setting['template']['input']);
 
             define("UPLOAD_PATH", $setting['upload']['path']);
 
